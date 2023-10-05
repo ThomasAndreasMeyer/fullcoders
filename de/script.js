7320cfe26e7cdf9b22b43c2499685d181f105006
ThomasAndreasMeyer <110246570+ThomasAndreasMeyer@users.noreply.github.com>
2023-10-05 14:14:07 +0200

    Add files via upload

--- de/script.js
+++ de/script.js
@@ -15,111 +15,31 @@ function showTextOnScroll() {
     });
 }
 
-// Führe die Funktion beim Laden der Seite und beim Scrollen aus
+// Führe die Funktion showTextOnScroll beim Laden der Seite und beim Scrollen aus
 window.addEventListener('load', showTextOnScroll);
 window.addEventListener('scroll', showTextOnScroll);
 
+// JavaScript-Funktion, um die Sprache basierend auf der Auswahl im Dropdown-Menü zu ändern
+function changeLanguage() {
+    var selectedLanguage = document.getElementById("language-select").value;
+    var page;
 
-       
-
-        // JavaScript-Funktion, um die Sprache basierend auf der Auswahl im Dropdown-Menü zu ändern
-        function changeLanguageIndex() {
-            var selectElement = document.getElementById("language-select");
-            var selectedLanguage = selectElement.value;
-            
-            // Hier können Sie den Code hinzufügen, um die Seite auf die ausgewählte Sprache umzuschalten,
-            // indem Sie die URL entsprechend ändern oder den Inhalt dynamisch laden.
-            
-            // Beispiel:
-            if (selectedLanguage === "en") {
-                // Wenn Englisch ausgewählt wurde, leite zur englischen Version weiter.
-                window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/en/index.html";
-            } else if (selectedLanguage === "de") {
-                // Wenn Deutsch ausgewählt wurde, leite zur deutschen Version weiter.
-                window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/de/index.html";
-            } else if (selectedLanguage === "pl") {
-                // Wenn Polnisch ausgewählt wurde, leite zur polnischen Version weiter.
-                window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/index.html";
-            }
-        }
-   
-// Führe die Funktion beim Laden der Seite und beim Scrollen aus
-window.addEventListener('load', showTextOnScroll);
-window.addEventListener('scroll', showTextOnScroll);
-
-
-     // JavaScript-Funktion, um die Sprache basierend auf der Auswahl im Dropdown-Menü zu ändern
-     function changeLanguageWebdev() {
-        var selectElement = document.getElementById("language-select");
-        var selectedLanguage = selectElement.value;
-        
-        // Hier können Sie den Code hinzufügen, um die Seite auf die ausgewählte Sprache umzuschalten,
-        // indem Sie die URL entsprechend ändern oder den Inhalt dynamisch laden.
-        
-        // Beispiel:
-        if (selectedLanguage === "en") {
-            // Wenn Englisch ausgewählt wurde, leite zur englischen Version weiter.
-            window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/en/webdevelopment.html";
-        } else if (selectedLanguage === "de") {
-            // Wenn Deutsch ausgewählt wurde, leite zur deutschen Version weiter.
-            window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/de/webdevelopment.html";
-        } else if (selectedLanguage === "pl") {
-            // Wenn Polnisch ausgewählt wurde, leite zur polnischen Version weiter.
-            window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/webdevelopment.html";
-        }
+    // Hier müssen Sie die Seite (page) je nach Bedarf festlegen, basierend auf dem Kontext
+    // Zum Beispiel, wenn es sich um eine Webentwicklungsseite handelt:
+    if (page === 'webdevelopment') {
+        page = 'webdevelopment';
+    } else if (page === 'seo') {
+        page = 'seo';
+    } else {
+        page = ''; // Geben Sie hier die Standardseite an, wenn keine spezifische Seite ausgewählt ist.
     }
 
+    var languageMap = {
+        'en': 'https://thomasandreasmeyer.github.io/fullcoders/en/',
+        'de': 'https://thomasandreasmeyer.github.io/fullcoders/de/',
+        'pl': 'https://thomasandreasmeyer.github.io/fullcoders/'
+    };
 
-        // Führe die Funktion beim Laden der Seite und beim Scrollen aus
-window.addEventListener('load', showTextOnScroll);
-window.addEventListener('scroll', showTextOnScroll);
-
-
-        // JavaScript-Funktion, um die Sprache basierend auf der Auswahl im Dropdown-Menü zu ändern
-        function changeLanguageSeo() {
-            var selectElement = document.getElementById("language-select");
-            var selectedLanguage = selectElement.value;
-            
-            // Hier können Sie den Code hinzufügen, um die Seite auf die ausgewählte Sprache umzuschalten,
-            // indem Sie die URL entsprechend ändern oder den Inhalt dynamisch laden.
-            
-           // Beispiel:
-        if (selectedLanguage === "en") {
-            // Wenn Englisch ausgewählt wurde, leite zur englischen Version weiter.
-            window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/en/seo.html";
-        } else if (selectedLanguage === "de") {
-            // Wenn Deutsch ausgewählt wurde, leite zur deutschen Version weiter.
-            window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/de/seo.html";
-        } else if (selectedLanguage === "pl") {
-            // Wenn Polnisch ausgewählt wurde, leite zur polnischen Version weiter.
-            window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/seo.html";
-        }
-    }
-
-
-        // Führe die Funktion beim Laden der Seite und beim Scrollen aus
-window.addEventListener('load', showTextOnScroll);
-window.addEventListener('scroll', showTextOnScroll);
-
-
-        // JavaScript-Funktion, um die Sprache basierend auf der Auswahl im Dropdown-Menü zu ändern
-        function changeLanguageOmnie() {
-            var selectElement = document.getElementById("language-select");
-            var selectedLanguage = selectElement.value;
-            
-            // Hier können Sie den Code hinzufügen, um die Seite auf die ausgewählte Sprache umzuschalten,
-            // indem Sie die URL entsprechend ändern oder den Inhalt dynamisch laden.
-            
-           // Beispiel:
-        if (selectedLanguage === "en") {
-            // Wenn Englisch ausgewählt wurde, leite zur englischen Version weiter.
-            window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/en/omnie.html";
-        } else if (selectedLanguage === "de") {
-            // Wenn Deutsch ausgewählt wurde, leite zur deutschen Version weiter.
-            window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/de/omnie.html";
-        } else if (selectedLanguage === "pl") {
-            // Wenn Polnisch ausgewählt wurde, leite zur polnischen Version weiter.
-            window.location.href = "https://thomasandreasmeyer.github.io/fullcoders/omnie.html";
-        }
-    }
-
+    var url = languageMap[selectedLanguage] + page + '.html';
+    window.location.href = url;
+}
