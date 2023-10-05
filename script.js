6b08fce60d8b4f4ad9e4d89a6762c6318cedf824
ThomasAndreasMeyer <110246570+ThomasAndreasMeyer@users.noreply.github.com>
2023-10-05 14:11:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,14 +15,25 @@ function showTextOnScroll() {
     });
 }
 
-
-
 // Führe die Funktion showTextOnScroll beim Laden der Seite und beim Scrollen aus
 window.addEventListener('load', showTextOnScroll);
 window.addEventListener('scroll', showTextOnScroll);
 
 // JavaScript-Funktion, um die Sprache basierend auf der Auswahl im Dropdown-Menü zu ändern
-function changeLanguage(selectedLanguage, page) {
+function changeLanguage() {
+    var selectedLanguage = document.getElementById("language-select").value;
+    var page;
+
+    // Hier müssen Sie die Seite (page) je nach Bedarf festlegen, basierend auf dem Kontext
+    // Zum Beispiel, wenn es sich um eine Webentwicklungsseite handelt:
+    if (page === 'webdevelopment') {
+        page = 'webdevelopment';
+    } else if (page === 'seo') {
+        page = 'seo';
+    } else {
+        page = ''; // Geben Sie hier die Standardseite an, wenn keine spezifische Seite ausgewählt ist.
+    }
+
     var languageMap = {
         'en': 'https://thomasandreasmeyer.github.io/fullcoders/en/',
         'de': 'https://thomasandreasmeyer.github.io/fullcoders/de/',
@@ -32,9 +43,3 @@ function changeLanguage(selectedLanguage, page) {
     var url = languageMap[selectedLanguage] + page + '.html';
     window.location.href = url;
 }
-
-// Sprache für die Webentwicklung ändern
-changeLanguage('en', 'webdevelopment'); // Englische Version der Webentwicklungsseite
-
-// Sprache für die SEO-Seite ändern
-changeLanguage('de', 'seo'); // Deutsche Version der SEO-Seite
