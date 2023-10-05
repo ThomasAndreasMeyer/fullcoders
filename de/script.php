89bf0dcb27f83e632123bb74633e6dd96b043c1b
ThomasAndreasMeyer <110246570+ThomasAndreasMeyer@users.noreply.github.com>
2023-10-05 13:34:56 +0200

    Add files via upload

---
+++ de/script.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $nachricht = $_POST["nachricht"];
+
+    // Empfänger-E-Mail-Adresse
+    $empfaenger = "thomas.meyer1984@web.de"; 
+
+    // Betreff und Nachricht
+    $betreff = "Kontaktanfrage von $name";
+    $nachricht = "Name: $name\n";
+    $nachricht .= "E-Mail: $email\n\n";
+    $nachricht .= "Nachricht:\n$nachricht";
+
+    // E-Mail-Versand
+    if (mail($empfaenger, $betreff, $nachricht)) {
+        echo "Die Nachricht wurde erfolgreich gesendet. Vielen Dank, $name!";
+    } else {
+        echo "Die Nachricht konnte nicht gesendet werden. Bitte versuchen Sie es später erneut.";
+    }
+}
+?>
